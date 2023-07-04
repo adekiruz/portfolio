2f34444b39f5ab93bbdeb410fcfd537c61366ac3
adekiruz <nrmuhamad@gmail.com>
2023-07-04 10:23:26 +0700

    refactor: :art: small refactor

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,18 +1,14 @@
-import { join } from 'path';
-import skeleton from '@skeletonlabs/skeleton/tailwind/skeleton.cjs';
-import forms from '@tailwindcss/forms';
-import typography from '@tailwindcss/typography';
+import { join } from 'path'
+import forms from '@tailwindcss/forms'
+import typography from '@tailwindcss/typography'
+import skeleton from '@skeletonlabs/skeleton/tailwind/skeleton.cjs'
 
-/** @type {import('tailwindcss').Config}*/
-const config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
 	darkMode: 'class',
-	content: [ './src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}') ],
-
+	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
 	theme: {
-		extend: {}
+		extend: {},
 	},
-
-	plugins: [ forms, typography, ...skeleton() ],
-};
-
-module.exports = config;
+	plugins: [forms,typography,...skeleton()],
+}
